1e5a78b9cbe1f824916c0d4d3cbc656f1f23aa5b
Mohit Patel <pmohit645@gmail.com>
2023-10-11 22:52:17 +0530

    New Update

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+    return (
+        <div className='rounded-full w-14 h-14 flex '>
+            <img className='rounded-full justify-center text-center' src="https://avatars.githubusercontent.com/u/109367447?v=4" alt="Logo" />
+        </div>
+    )
+}
+
+export default Logo
